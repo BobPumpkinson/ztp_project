24f42163a9855e65c10b3cd2db589faa253c9833
BobPumpkinson <106379137+BobPumpkinson@users.noreply.github.com>
2025-06-30 16:27:22 +0200

    Fixed password change

---
+++ app/src/Form/Type/NewPasswordType.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * New Password type.
+ */
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Class NewUserType.
+ */
+class NewPasswordType extends AbstractType
+{
+    /**
+     * Builds the form.
+     *
+     * This method is called for each type in the hierarchy starting from the
+     * top most type. Type extensions can further modify the form.
+     *
+     * @param FormBuilderInterface $builder The form builder
+     * @param array<string, mixed> $options Form options
+     *
+     * @see FormTypeExtensionInterface::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add(
+            'password',
+            PasswordType::class,
+            [
+                'label' => 'label.password',
+                'required' => true,
+                'mapped' => false,
+                'attr' => ['max_length' => 180],
+            ]
+        );
+    }
+
+    /**
+     * Returns the prefix of the template block name for this type.
+     *
+     * The block prefix defaults to the underscored short class name with
+     * the "Type" suffix removed (e.g. "UserProfileType" => "user_profile").
+     *
+     * @return string The prefix of the template block name
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'user';
+    }
+}
